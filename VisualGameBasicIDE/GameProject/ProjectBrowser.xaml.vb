bbf9e4ac995dad8f3b26467c8230b3ca6ca4bcb3
gracepriest <melvinkempjr3@outlook.com>
2025-01-31 05:07:12 -0500

    update

---
+++ VisualGameBasicIDE/GameProject/ProjectBrowser.xaml.vb
@@ -0,0 +1,17 @@
+﻿Public Class ProjectBrowser
+    Private Sub Tab_New_MouseDown(sender As Object, e As MouseButtonEventArgs)
+        CreateButton.Content = "New"
+    End Sub
+
+    Private Sub Tab_Exsisting_MouseDown(sender As Object, e As MouseButtonEventArgs)
+        CreateButton.Content = "Open"
+    End Sub
+
+    Private Sub Tab_Recent_MouseDown(sender As Object, e As MouseButtonEventArgs)
+        CreateButton.Content = "Open"
+    End Sub
+
+    Private Sub ExitButton_Click(sender As Object, e As RoutedEventArgs)
+        Me.Close()
+    End Sub
+End Class
---
+++ VisualGameBasicIDE/MainWindow.xaml.vb
@@ -0,0 +1,12 @@
+﻿Class MainWindow
+    Private Sub MainWindow_Loaded(sender As Object, e As RoutedEventArgs) Handles Me.Loaded
+        Dim projectBrowser As New ProjectBrowser
+        If projectBrowser.ShowDialog() = False Then
+            Application.Current.Shutdown()
+        Else
+
+        End If
+    End Sub
+
+
+End Class
